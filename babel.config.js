705ce0345f627dab9723d44406c131e901e335e2
Elyas Asmad <elyasasmadz@gmail.com>
2022-02-14 02:46:18 +0800

    First Commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
+const path = require('path');
+const pak = require('./package.json')
+
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json']
+        }
+      ],
+      'react-native-reanimated/plugin'
+    ]
   };
 };
